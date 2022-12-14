1f6c907a222871002b5b04b0429d192949033ddb
Ron Cleveland <rcleveland30@gmail.com>
2022-12-13 21:50:36 -0600

    updates to About page, header change

--- react-ui/src/views/about.jsx
+++ react-ui/src/views/about.jsx
@@ -4,10 +4,17 @@ import aboutPic from '../assets/aboutpic.jpg'
 function About ({}) {
     return (
         <div className="y-wrap">
-            <div>
-                <h2>Meet the Creator</h2>
+            <div className="about-h">
+                <h2>Ronarro L. Cleveland, II</h2>
+                <h3>Creator of American University Football Weekend</h3>
+            </div>
+            <div className="about-pg">
                 <img src={aboutPic} className="about-pic" alt="Ronarro Cleveland, II" />
-                <h3>Ronarro L. Cleveland, II</h3>
+            </div>
+            <div className="about-p">
+                <p className="">As a fan of college football, I wanted to provide something to the community. American University Football Weekend (because I'm sure College Football Saturday was taken) is a place to find information for your favorite and other teams you wish to follow!</p>
+                <br />
+                <p>AUFW is my final capstone project with DigitalCrafts. It was built with React.js and Redux. I enjoyed creating components to make my imagination become reality.</p>
             </div>
         </div>
     );
